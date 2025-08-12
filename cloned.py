a445f0ab76ec1d3d3775d0bfe3718cb4ce7cf337
prakarshm <prakarshm180703@gmail.com>
2025-08-12 22:56:43 +0530

    new code

---
+++ cloned.py
@@ -0,0 +1,10 @@
+def new_func(a,b):
+    print(a-b)
+
+def another_func(x, y):
+    return x * y    
+
+def main():
+    new_func(10, 5)
+    result = another_func(3, 4)
+    print("Result of multiplication:", result)
